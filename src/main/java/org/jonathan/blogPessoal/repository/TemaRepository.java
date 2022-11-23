2b6a9f4d092bd0a2d07e028d5ab3b4ddaddbb102
unknown <jonathansoliveira7@gmail.com>
2022-11-22 21:10:36 -0300

    budega

---
+++ src/main/java/org/jonathan/blogPessoal/repository/TemaRepository.java
@@ -0,0 +1,12 @@
+package org.jonathan.blogPessoal.repository;
+
+import java.util.List;
+
+import org.jonathan.blogPessoal.model.Tema;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TemaRepository extends JpaRepository<Tema, Long> {
+ public List<Tema> findAllByDescricaoContainingIgnoreCase(String drescricao);
+ 
+	
+}
